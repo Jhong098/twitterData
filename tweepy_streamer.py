71d9f1cd91556a3299a6469d8cd9668d1a7d6787
Jason Hong <r25hong@edu.uwaterloo.ca>
2018-10-27 14:36:28 -0400

    add sentiment analysis column

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -83,7 +83,20 @@ class TwitterListener(StreamListener):
         print(status)
 
 class TweetAnalyzer():
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+_)", " ", tweet).split())
     
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
         df['id'] = np.array([tweet.id for tweet in tweets])
@@ -103,26 +116,9 @@ if __name__ == "__main__":
     tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
     
     df = tweet_analyzer.tweets_to_data_frame(tweets)
+    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
 
-    # Get average length over all tweets
-    print(np.mean(df['len']))
+    print(df.head(10))
 
-    # Get the number of likes for the most liked tweet
-    print(np.max(df['likes']))
 
-    # Get the number of retweets for the most retweeted tweet
-    print(np.max(df['retweets']))
-
-    # Time Series
-    # time_likes = pd.Series(data=df['likes'].values, index=df['date'])
-    # time_likes.plot(figsize=(16, 4), color='r')
-    # plt.show()
-
-    time_likes = pd.Series(data=df['likes'].values, index=df['date'])
-    time_likes.plot(figsize=(16, 4), label="likes", legend=True)
-
-    time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
-    time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
-
-    plt.show()
     
