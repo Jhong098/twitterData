f9c8e188182c90ece8c004a7bd746c94e1c380fa
Jhong098 <Jhong098@gmail.com>
2018-10-27 02:15:47 -0400

    twitter api setup

---
+++ twitter_cred.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter API
+ACCESS_TOKEN = "57744873-nvKTeX5vfsnJAzUGgUKWfgdsWudRBGHz1VlzvcA4p"
+ACCESS_TOKEN_SECRET = "U5gN0Vx0IZeGVu6v1hIr4mYoKAeiXhtm1wrVY133Qsjg6"
+CONSUMER_KEY = "ZXqXafxsfkUyF744O9Fm5RS9c"
+CONSUMER_SECRET = "JTUMpddpoqou9ipGCOPN7poGmlcVD4ZfHuVQKQbaHnFKGBnGmV"
